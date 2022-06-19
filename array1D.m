9f70c5590534864954983d35036400deaf248bcd
lrc-radar <107789763+lrc-radar@users.noreply.github.com>
2022-06-19 16:21:44 +0800

    Add files via upload
    Drawing the  radiation pattern of  antenna array  ( array factor) using MATLAB

---
+++ array1D.m
@@ -0,0 +1,25 @@
+clear;
+clc;
+close all;
+%% Basic Electromagnetic Parameters
+Frequency = 10e9;
+Lightspeed = physconst('LightSpeed');
+Wavelength = Lightspeed/Frequency;
+Wavenumber = 2*pi/Wavelength;
+
+%% Array Parameters
+N =12;
+X = (1:N)*Wavelength/2;
+I =  ones(1,N);
+alpha = zeros(1,N);
+
+%% ArrayFactor Samping
+Ns =500;% Sampling number
+theta = linspace(-90,90,Ns);
+E =zeros(1,Ns);
+
+for num = 1:Ns
+    E(num)=sum(I.*exp(1j*(Wavenumber*X*sind(theta(num))+alpha)))+1e-3;
+end
+%% plot figure
+plot(theta,db(E));
