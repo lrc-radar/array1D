76a5a832a5a7065029628c1466100ad7c5bd1af0
lrc-radar <107789763+lrc-radar@users.noreply.github.com>
2022-07-26 21:00:43 +0800

    Update array1D_Scan.m

--- array1D_Scan.m
+++ array1D_Scan.m
@@ -10,8 +10,6 @@ Wavenumber = 2*pi/Wavelength;
 %% Array Parameters
 N =33;
 X = (0:(N-1))*Wavelength/2;
-% xrand = cumsum(rand(1,(N-1)));
-% X=[0,xrand]/max(xrand)*(N-1)*Wavelength;
 I =  ones(1,N);
 theta0 = 45;
 alpha = -Wavenumber*X*sind(theta0);
@@ -25,25 +23,8 @@ for num = 1:Ns
 end
 %% plot figure
 E_dB = db(E)-max(db(E));
-% figure()
-% plot(theta,db(E),'LineWidth',2);
-% grid on
 figure()
 plot(theta,E_dB,'LineWidth',2);%normalized
 axis tight
 grid on 
 xlabel('\theta(\circ)');ylabel('(dB)');
-% set(gca,'FontSize',14);
-% figure()
-% polarplot(deg2rad(theta),abs(E),'LineWidth',2);
-
-%% calc PSLL
-[peaks,locs] = findpeaks(E_dB,'SortStr','descend');
-psll = peaks(2)-peaks(1);
-
-hold on 
-scatter(theta(locs(2)),psll,100,'rv');
-hold off
-hold on 
-scatter(theta(locs(1)),peaks(1),100,'rv');
-hold off
